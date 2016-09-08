9d72a2ec60098a558dbb542d9888322253f4fd73
lxlenovostar <lxlenovostar@gmail.com>
2016-09-08 17:08:58 +0800

    python性能测试模块使用

---
+++ python/ptest_md5_sha1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import timeit
+
+t = timeit.Timer(
+"""
+import hashlib
+for line in url_paths:
+    h = hashlib.md5(line).hexdigest()
+#    h = hashlib.sha1(line).hexdigest()
+#    h = hashlib.sha256(line).hexdigest()
+#    h = hashlib.sha512(line).hexdigest()
+"""
+,
+"""
+url_paths = []
+f = open('urls.txt', 'r')
+for l in f.readlines():
+    url_paths.append(l)
+f.close()
+"""
+)
+
+print t.repeat(repeat=1, number=10)
