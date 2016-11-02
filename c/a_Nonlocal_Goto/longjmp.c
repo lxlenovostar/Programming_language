34ef8b38424a553c151dd87d6559d5efde683646
root <root@localhost.localdomain>
2016-11-02 02:41:22 -0700

    add

--- c/a_Nonlocal_Goto/longjmp.c
+++ c/a_Nonlocal_Goto/longjmp.c
@@ -13,6 +13,22 @@
 #include <setjmp.h>
 #include "tlpi_hdr.h"
 
+/*
+ 关于env的没有理解。
+ Along with other information, env stores a copy of the program counter register
+(which points to the currently executing machine-language instruction) and the
+stack pointer register (which marks the top of the stack) at the time of the call to
+setjmp(). This information enables the subsequent longjmp() call to accomplish two
+key steps:
+z Strip off the stack frames for all of the intervening functions on the stack
+between the function calling longjmp() and the function that previously called
+setjmp(). This procedure is sometimes called “unwinding the stack,” and is
+accomplished by resetting the stack pointer register to the value saved in the
+env argument.
+z Reset the program counter register so that the program continues execution
+from the location of the initial setjmp() call. Again, this is accomplished using
+the value saved in env.
+ */
 static jmp_buf env;
 
 static void
@@ -25,6 +41,7 @@ static void
 f1(int argc)
 {
     if (argc == 1)
+        //longjmp(env, 0);
         longjmp(env, 1);
     f2();
 }
