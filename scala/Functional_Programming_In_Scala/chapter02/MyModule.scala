5584f225b2b86b96dcb83cd18c20a39c79fb9522
lxlenovostar <lxlenovostar@gmail.com>
2016-11-05 23:44:43 +0800

    add

---
+++ scala/Functional_Programming_In_Scala/chapter02/MyModule.scala
@@ -0,0 +1,17 @@
+/**
+  * Created by lix on 16-11-5.
+  */
+object MyModule {
+  def abs(n: Int): Int =
+    if (n < 0) -n
+    else n
+
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  def main(args: Array[String]): Unit =
+    println(formatAbs(-42))
+
+}
