737dc894ba5995f3d64383a912dea57eb1da8a2a
lxlenovostar <lxlenovostar@gmail.com>
2017-02-13 17:38:06 +0800

    理解k-mean聚类的程序实现

--- python/Programming_Collective_Intelligence/ch03/clusters.py
+++ python/Programming_Collective_Intelligence/ch03/clusters.py
@@ -179,4 +179,61 @@ def drawnode(draw, clust, x, y, scaling, labels):
 		# If this is an endpoint, draw the item label
 		draw.text((x+5,y-7),labels[clust.id],(0,0,0))
 
+def rotatematrix(data):
+	newdata=[]
 
+	# 行列转换的代码
+	for i in range(len(data[0])):
+		newrow = [data[j][i] for j in range(len(data))]
+		newdata.append(newrow)
+	
+	return newdata
+
+import random
+
+def kcluster(rows, distance = pearson, k = 4):
+	# Determine the minimum and maximum values for each point
+	# 这里的最大最小值是针对所有博客的同一个单词
+	ranges=[(min([row[i] for row in rows]), max([row[i] for row in rows])) for i in range(len(rows[0]))]
+
+	# Create k randomly placed centroids
+	#for i in range(len(rows[0])) 
+	#	random.random()*(ranges[i][1]-ranges[i][0]) + ranges[i][0] 
+	clusters=[[random.random()*(ranges[i][1]-ranges[i][0])+ranges[i][0] for i in range(len(rows[0]))] for j in range(k)]
+	
+	lastmatches = None
+	for t in range(100):
+		print 'Iteration %d' % t
+		bestmatches=[[] for i in range(k)]
+
+		# Find which centroid is the closest for each row
+		for j in range(len(rows)):
+			row = rows[j]
+			bestmatch = 0
+			for i in range(k):
+				d = distance(clusters[i], row)
+				if d < distance(clusters[bestmatch], row):
+					bestmatch = i
+			bestmatches[bestmatch].append(j)
+
+		# If the results are the same as last time, this is complete
+		if bestmatches == lastmatches: 
+			break
+
+		lastmatches = bestmatches
+
+		# Move the centroids to the average of their members
+		for i in range(k):
+			avgs=[0.0]*len(rows[0])
+			if len(bestmatches[i])>0:
+				for rowid in bestmatches[i]:
+					for m in range(len(rows[rowid])):
+						avgs[m] += rows[rowid][m]
+
+				for j in range(len(avgs)):
+					avgs[j] /= len(bestmatches[i])
+				clusters[i]=avgs
+	
+	# Because this function uses random centroids to start with, the order of the results
+	# returned will almost always be different. 
+	return bestmatches
