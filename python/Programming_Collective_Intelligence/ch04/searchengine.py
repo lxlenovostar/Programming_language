f12a34c6e3b600c06759fd711ee6a1372d9237a5
lxlenovostar <lxlenovostar@gmail.com>
2017-02-15 16:04:35 +0800

    代码需要理解

--- python/Programming_Collective_Intelligence/ch04/searchengine.py
+++ python/Programming_Collective_Intelligence/ch04/searchengine.py
@@ -4,41 +4,90 @@
 import urllib2
 from BeautifulSoup import *
 from urlparse import urljoin
+from pysqlite2 import dbapi2 as sqlite
 
 # Create a list of words to ignore
 ignorewords=set(['the','of','to','and','a','in','is','it'])
 
 class crawler:
     # Initialize the crawler with the name of database
-    def __init__(self,dbname):
-        pass
+    def __init__(self, dbname):
+		self.con = sqlite.connect(dbname)
 
     def __del__(self):
-        pass
+		self.con.close()
 
     def dbcommit(self):
-        pass
+		self.con.commit()
 
     # Auxilliary function for getting an entry id and adding
     # it if it's not present
-    def getentryid(self,table,field,value,createnew=True):
-        return None
+    def getentryid(self, table, field, value, createnew=True):
+		cur =self.con.execute("select rowid from %s where %s='%s'" % (table,field,value))
+		res = cur.fetchone( )
+
+		if res == None:
+			cur=self.con.execute("insert into %s (%s) values ('%s')" % (table,field,value))
+			return cur.lastrowid
+		else:
+			return res[0]
 
     # Index an individual page
     def addtoindex(self,url,soup):
-        print 'Indexing %s' % url
+		if self.isindexed(url): 
+			return
+
+		print 'Indexing '+url
+
+		# Get the individual words
+		text = self.gettextonly(soup)
+		words = self.separatewords(text)
+
+		# Get the URL id
+		urlid = self.getentryid('urllist', 'url', url)
+
+		# Link each word to this url
+		for i in range(len(words)):
+			word = words[i]
+			if word in ignorewords: 
+				continue
+			wordid = self.getentryid('wordlist', 'word', word)
+			self.con.execute("insert into wordlocation(urlid,wordid,location) \
+				values (%d,%d,%d)" % (urlid,wordid,i))
 
     # Extract the text from an HTML page (no tags)
-    def gettextonly(self,soup):
-        return None
+    def gettextonly(self, soup):
+		v = soup.string
+
+		if v == None:
+			c = soup.contents
+			resulttext = ''
+
+			for t in c:
+				subtext = self.gettextonly(t)
+				resulttext += subtext + '\n'
+
+			return resulttext
+		else:
+			return v.strip()
 
     # Separate the words by any non-whitespace character
     def separatewords(self,text):
-        return None
+		# \W 匹配任何非单词字符。等价于 '[^A-Za-z0-9_]'
+		splitter=re.compile('\\W*')
+		return [s.lower( ) for s in splitter.split(text) if s!='']
 
     # Return true if this url is already indexed
     def isindexed(self,url):
-        return False
+		u=self.con.execute("select rowid from urllist where url='%s'" % url).fetchone()
+
+		if u != None:
+			# Check if it has actually been crawled
+			v = self.con.execute('select * from wordlocation where urlid=%d' % u[0]).fetchone()
+			if v!= None: 
+				return True
+
+		return False
 
     # Add a link between two pages
     def addlinkref(self,urlFrom,urlTo,linkText):
@@ -85,4 +134,16 @@ class crawler:
 
     # Create the database tables
     def createindextables(self):
-        pass
+		self.con.execute('create table urllist(url)')
+		self.con.execute('create table wordlist(word)')
+		self.con.execute('create table wordlocation(urlid,wordid,location)')
+		self.con.execute('create table link(fromid integer,toid integer)')
+		self.con.execute('create table linkwords(wordid,linkid)')
+		# some indices to speed up searching
+		self.con.execute('create index wordidx on wordlist(word)')
+		self.con.execute('create index urlidx on urllist(url)')
+		self.con.execute('create index wordurlidx on wordlocation(wordid)')
+		self.con.execute('create index urltoidx on link(toid)')
+		self.con.execute('create index urlfromidx on link(fromid)')
+		self.dbcommit()
+
