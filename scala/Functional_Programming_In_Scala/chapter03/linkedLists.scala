e2a40ff882c4d3a0b5c997b418b4b66a911ec92e
lxlenovostar <lxlenovostar@gmail.com>
2017-03-03 15:00:39 +0800

    learning data sharing

--- scala/Functional_Programming_In_Scala/chapter03/linkedLists.scala
+++ scala/Functional_Programming_In_Scala/chapter03/linkedLists.scala
@@ -29,6 +29,72 @@ case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
+    /*
+     * It's generally good practice when pattern matching to use `_` for any variables 
+     * you don't intend to use on the right hand side of a pattern. This makes it clear 
+     * the value isn't relevant.
+     * */
+	def tail[A](list: List[A]): List[A] = list match {
+		case Nil => Nil
+		case Cons(_, xs) => xs
+	}
+
+     /*
+      * 这个使用了递归，有可能导致堆栈溢出。
+      * Note that we're copying the entire list up until the last element. Besides being inefficient, 
+      * the natural recursive solution will use a stack frame for each element of the list, which can 
+      * lead to stack overflows for large lists (can you see why?). With lists, it's common to use a 
+      * temporary, mutable buffer internal to the function (with lazy lists or streams, which we discuss 
+      * in chapter 5, we don't normally do this). So long as the buffer is allocated internal to the function, 
+      * the mutation is not observable and RT is preserved.
+      *
+      * Another common convention is to accumulate the output list in reverse order, then reverse it at the end, 
+      * which doesn't require even local mutation. We'll write a reverse function later in this chapter.
+      *
+      * */
+    def init[A](l: List[A]) : List[A] = 
+         l match {
+           case Nil => sys.error("init of empty list")
+           case Cons(_, Nil) => Nil
+           case Cons(h, t) => Cons(h,init(t))
+         }
+
+    def init2[A](l: List[A]): List[A] = {
+        import collection.mutable.ListBuffer
+        val buf = new ListBuffer[A]
+        @annotation.tailrec
+        def go(cur: List[A]): List[A] = cur match {
+            case Nil => sys.error("init of empty list")
+            case Cons(_, Nil) => List(buf.toList: _*)
+            case Cons(h,t) => buf += h; go(t)
+        }
+        go(l)
+    }
+
+	def drop[A](l: List[A], n: Int): List[A] = 
+        if (n <= 0) l
+        else l match {
+            case Nil => Nil 
+            case Cons(_, t) => drop(t, n-1)
+        }
+
+    /*
+     * Somewhat overkill, but to illustrate the feature we're using a _pattern guard_, 
+     * to only match a `Cons` whose head satisfies our predicate, `f`. The syntax is to 
+     * add `if <cond>` after the pattern, before the `=>`, where `<cond>` can use any of 
+     * the variables introduced by the pattern.
+     * */
+    def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match { 
+        case Cons(h, t) if f(h)  => dropWhile(t, f)
+        case _ => l
+    }
+
+
+    def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
+        case Nil => a2
+        case Cons(h,t) => Cons(h, append(t, a2))
+    }
+
 	def sum(ints: List[Int]): Int = ints match {
 		case Nil => 0
 		case Cons(x,xs) => x + sum(xs)
