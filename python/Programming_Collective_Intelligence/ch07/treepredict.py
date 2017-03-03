2ef3eb85469a7030732c071cf79978d9fa8bbc6c
lxlenovostar <lxlenovostar@gmail.com>
2017-03-03 16:57:11 +0800

    learning 决策树

---
+++ python/Programming_Collective_Intelligence/ch07/treepredict.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-  
+
+my_data=[['slashdot','USA','yes',18,'None'],
+        ['google','France','yes',23,'Premium'],
+        ['digg','USA','yes',24,'Basic'],
+        ['kiwitobes','France','yes',23,'Basic'],
+        ['google','UK','no',21,'Premium'],
+        ['(direct)','New Zealand','no',12,'None'],
+        ['(direct)','UK','no',21,'Basic'],
+        ['google','USA','no',24,'Premium'],
+        ['slashdot','France','yes',19,'None'],
+        ['digg','USA','no',18,'None'],
+        ['google','UK','no',18,'None'],
+        ['kiwitobes','UK','no',19,'None'],
+        ['digg','New Zealand','yes',12,'Basic'],
+        ['slashdot','UK','no',21,'None'],
+        ['google','UK','yes',18,'Basic'],
+        ['kiwitobes','France','yes',19,'Basic']]
+
+class decisionnode:
+    def __init__(self,col=-1,value=None,results=None,tb=None,fb=None):
+        self.col=col
+        self.value=value
+        self.results=results
+        self.tb=tb
+        self.fb=fb
+
+# Divides a set on a specific column. Can handle numeric
+# or nominal values
+def divideset(rows,column,value):
+    # Make a function that tells us if a row is in
+    # the first group (true) or the second group (false)
+    split_function=None
+    if isinstance(value,int) or isinstance(value,float):
+        split_function=lambda row:row[column]>=value
+    else:
+        split_function=lambda row:row[column]==value
+        # Divide the rows into two sets and return them
+        set1=[row for row in rows if split_function(row)]
+        set2=[row for row in rows if not split_function(row)]
+        return (set1,set2)
+
+# Create counts of possible results (the last column of
+# each row is the result)
+def uniquecounts(rows):
+    results={}
+    for row in rows:
+        # The result is the last column
+        r=row[len(row)-1]
+        if r not in results: results[r]=0
+        results[r]+=1
+    return results
+
+# 在CART算法中, 基尼不纯度表示一个随机选中的样本在子集中被分错的可能性。
+# 基尼不纯度为这个样本被选中的概率乘以它被分错的概率。当一个节点中所有样本都是一个类时，
+# 基尼不纯度为零。
+#
+# Probability that a randomly placed item will
+# be in the wrong category
+def giniimpurity(rows):
+    total = len(rows)
+    counts = uniquecounts(rows)
+    imp = 0
+
+    print total
+    print counts 
+
+    for k1 in counts:
+        p1 = float(counts[k1])/total
+        for k2 in counts:
+            if k1 == k2: continue
+            p2 = float(counts[k2])/total
+            # 选中的概率乘以错误选择的概率
+            imp += p1*p2
+    return imp
+
