a5c5a1de7c0b2e817af3ef5bdabfcd738ad08e91
lxlenovostar <lxlenovostar@gmail.com>
2017-03-13 15:24:59 +0800

    完成决策树

--- python/Programming_Collective_Intelligence/ch07/treepredict.py
+++ python/Programming_Collective_Intelligence/ch07/treepredict.py
@@ -163,26 +163,26 @@ def classify(observation, tree):
                 branch = tree.fb
         return classify(observation, branch)
 
-def prune(tree,mingain):
+def prune(tree, mingain):
     # If the branches aren't leaves, then prune them
-    if tree.tb.results==None:
-        prune(tree.tb,mingain)
-    if tree.fb.results==None:
-        prune(tree.fb,mingain)
+    if tree.tb.results == None:
+        prune(tree.tb, mingain)
+    if tree.fb.results == None:
+        prune(tree.fb, mingain)
             
     # If both the subbranches are now leaves, see if they
     # should merged
-    if tree.tb.results!=None and tree.fb.results!=None:
+    if tree.tb.results != None and tree.fb.results != None:
         # Build a combined dataset
-        tb,fb=[],[]
-        for v,c in tree.tb.results.items( ):
-            tb+=[[v]]*c
-        for v,c in tree.fb.results.items( ):
-            fb+=[[v]]*c
+        tb ,fb= [], []
+        for v,c in tree.tb.results.items():
+            tb += [[v]]*c
+        for v,c in tree.fb.results.items():
+            fb += [[v]]*c
         
         # Test the reduction in entropy
-        delta=entropy(tb+fb)-(entropy(tb)+entropy(fb)/2)
-        if delta<mingain:
+        delta = entropy(tb+fb)-(entropy(tb)+entropy(fb)/2)
+        if delta < mingain:
             # Merge the branches
             tree.tb,tree.fb=None,None
             tree.results=uniquecounts(tb+fb)
