3064c81ecbc4a1714883f6986e91407050f3c38e
lxlenovostar <lxlenovostar@gmail.com>
2017-05-16 17:47:54 +0800

    learn go

---
+++ Go/The_Go_Programming_Language/ch01/ex1.2.go
@@ -0,0 +1,18 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	for tmp, arg := range os.Args[1:] {
+		fmt.Printf("%v\n", strconv.Itoa(tmp) + " " + arg)
+	}
+}
+
+//!-
