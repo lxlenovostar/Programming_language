2d27052fd9c0aafcad0fafa1b6ec2887718ce186
lxlenovostar <lxlenovostar@gmail.com>
2017-06-02 14:05:06 +0800

    增加访问控制

--- cpp/cppPrimer5/ch07/Sales_data.h
+++ cpp/cppPrimer5/ch07/Sales_data.h
@@ -1,15 +1,35 @@
 #include <string>
 
-struct Sales_data {
-		// new members: operations on Sales_data objects
-		std::string isbn() const { return bookNo; }
-		Sales_data& combine(const Sales_data&);
-		double avg_price() const;
-		
-		// data members are unchanged from § 2.6.1 (p. 72)
-		std::string bookNo;
-		unsigned units_sold = 0;
-		double revenue = 0.0;
+class Sales_data {
+public:
+	Sales_data() = default;
+	Sales_data(const std::string &s, unsigned n, double p): bookNo(s), units_sold(n), revenue(p*n) 
+	{ 
+	}
+
+	Sales_data(const std::string &s): bookNo(s) 
+	{ 
+	}
+
+	Sales_data(std::istream&);
+
+	// new members: operations on Sales_data objects
+	std::string isbn() const 
+	{ 
+		return bookNo; 
+	}
+
+	Sales_data &combine(const Sales_data&);
+private:	
+	double avg_price() const 
+	{
+		return units_sold ? revenue/units_sold : 0;
+	}
+
+	// data members are unchanged from § 2.6.1 (p. 72)
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
 };
 
 // nonmember Sales_data interface functions
