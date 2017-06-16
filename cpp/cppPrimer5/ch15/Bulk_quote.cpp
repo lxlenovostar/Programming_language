fc8a68706d98b72f8d1f5fc16eb0057367876022
lxlenovostar <lxlenovostar@gmail.com>
2017-06-16 11:27:43 +0800

    1.增加派生类访问基类成员

--- cpp/cppPrimer5/ch15/Bulk_quote.cpp
+++ cpp/cppPrimer5/ch15/Bulk_quote.cpp
@@ -1,9 +1,25 @@
 class Bulk_quote : public Quote { // Bulk_quote inherits from Quote
 		Bulk_quote() = default;
 		Bulk_quote(const std::string&, double, std::size_t, double);
+		Bulk_quote(const std::string& book, double p, std::size_t qty, double disc) :
+				Quote(book, p), min_qty(qty), discount(disc) 
+		{ }
+
+		// as before
+		// };
 		// overrides the base version in order to implement the bulk purchase discount policy
 		double net_price(std::size_t) const override;
 	private:
 		std::size_t min_qty = 0; // minimum purchase for the discount to apply
 		double discount = 0.0; // fractional discount to apply
 };
+
+// if the specified number of items are purchased, use the discounted price
+double Bulk_quote::net_price(size_t cnt) const
+{
+	if (cnt >= min_qty)
+		return cnt * (1 - discount) * price;
+	else
+		return cnt * price;
+}
+
