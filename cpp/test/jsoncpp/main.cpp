3496bb8b15e7f83b813198be3ad6d09d4e67244d
lxlenovostar <lxlenovostar@gmail.com>
2017-07-12 18:52:53 +0800

    1.依然存在问题，需要调试

--- cpp/test/jsoncpp/main.cpp
+++ cpp/test/jsoncpp/main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int main()   
 {  
+	/*
 	std::cout <<  "example 1:" << std::endl;  
 	std::string test = "{\"id\":1,\"name\":\"hello\"}";  
     Json::Reader reader;  
@@ -19,4 +20,23 @@ int main()
 	} else {  
 		std::cout << "parse error" << std::endl;  
 	}  
-}  
+	*/
+
+	std::cout << "example 2:" << std::endl;  
+  	string test = "{\"array\":[{\"id\":1,\"name\":\"hello\"},{\"id\":2,\"name\":\"world\"}]}";  
+    Json::Reader reader;  
+  	Json::Value value;  
+
+    if (reader.parse(test, value)) {  
+		const Json::Value arrayObj = value["array"];  
+	    for (size_t i = 0; i < arrayObj.size(); i++) {  
+			int id = (arrayObj[i])["id"].asInt();  
+		    string name = (arrayObj[i])["name"].asString();  
+			std::cout << id << " " << name << std::endl;  
+		} 
+	}
+	else {  
+		std::cout << "parse error" << std::endl;  
+	}  
+	
+}
