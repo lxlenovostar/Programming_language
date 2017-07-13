d7402821261af5f9c46de9d176f71d46f42743f4
lxlenovostar <lxlenovostar@gmail.com>
2017-07-13 16:44:01 +0800

    learn glog

--- cpp/test/glog/main.cpp
+++ cpp/test/glog/main.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
-#include "glog/logging.h"   
+#include <glog/logging.h>  
 
 using namespace std;
 
+//g++ main.cpp -o main -g -Wall -Wl,-Bstatic -lglog -Wl,-Bdynamic -lpthread
+
 int main(int argc, char** argv) 
 {
-	google::InitGoogleLogging(argv[0]); 
+
 	FLAGS_log_dir=".";   
+	google::InitGoogleLogging(argv[0]); 
+
+	google::SetLogDestination(google::GLOG_INFO, "INFO.");
+	google::SetLogDestination(google::GLOG_WARNING, "WARNING.");
+	google::SetLogDestination(google::GLOG_ERROR, "ERROR.");
+	google::SetLogDestination(google::GLOG_FATAL, "FATAL.");
+
 	LOG(INFO) << "hello glog";     
+	int num_cookies = 2;
+	LOG_IF(INFO, num_cookies > 10) << "Got lots of cookies";
+	LOG(ERROR) << "what't error.";     
+	LOG(FATAL) << "what't error.";     
+
 
 	return 0;
 }
