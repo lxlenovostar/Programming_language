8d310b0f09142b4c102470458fb30aa9a08367fc
lxlenovostar <lxlenovostar@gmail.com>
2017-07-13 14:36:28 +0800

    learn string

---
+++ cpp/cppPrimer5/ch05/rw_string.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*
+	string s1, s2;
+	
+	//string对象会自动忽略开头的空白（空格符、换行符、制表符等）并从
+	//第一个真正的字符开始读起，直到遇见下一个空白为止。
+	cin >> s1 >> s2;
+	cout << s1 << s2 << endl;
+	*/
+
+	string word;
+	//while(cin >> word) 
+	while(getline(cin, word)) 
+		cout << word << endl;
+
+	return 0;
+
+}
