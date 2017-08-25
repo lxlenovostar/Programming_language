2d3e6acd39713fa96987d3e54e8a4b8f603a9eee
lxlenovostar <lxlenovostar@gmail.com>
2017-08-25 10:35:55 +0800

    1.learn tie

---
+++ cpp/test/boost/tier/tie.cpp
@@ -0,0 +1,17 @@
+#include <boost/tuple/tuple.hpp> 
+#include <boost/tuple/tuple_io.hpp> 
+#include <string> 
+#include <iostream> 
+
+int main() 
+{ 
+	typedef boost::tuple<std::string&, std::string&, int&> person; 
+
+    std::string firstname = "Boris"; 
+  	std::string surname = "Schaeling"; 
+    int shoesize = 43; 
+  	person p = boost::tie(firstname, surname, shoesize); 
+  	std::cout << p << std::endl; 
+    surname = "Becker"; 
+  	std::cout << p << std::endl; 
+} 
