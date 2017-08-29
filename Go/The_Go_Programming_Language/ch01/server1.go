4f6134f70be073d6c4697c610c1e6c7a3990d56d
lxlenovostar <lxlenovostar@gmail.com>
2017-08-29 13:54:53 +0800

    1.learn go

---
+++ Go/The_Go_Programming_Language/ch01/server1.go
@@ -0,0 +1,28 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 19.
+//!+
+
+// Server1 is a minimal "echo" server.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// 发送到这个服务端请求是一个http.Request类型的对象，这个对象中
+// 包含了请求中的一系列相关字段。
+// handler echoes the Path component of the requested URL.
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+//!-
