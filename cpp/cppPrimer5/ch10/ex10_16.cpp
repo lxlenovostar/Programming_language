80c4447850bbcbc8e74244b6e3ab4cf088564c0b
lxlenovostar <lxlenovostar@gmail.com>
2017-10-23 22:50:17 +0800

    1.learn lambda

---
+++ cpp/cppPrimer5/ch10/ex10_16.cpp
@@ -0,0 +1,66 @@
+//!
+//! @author @Yue Wang @pezy
+//! @date   02.12.2014
+//!
+//! Exercise 10.16:
+//! Write your own version of the biggies function using lambdas.
+//!
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+//TODO 参考 http://www.cplusplus.com/reference/algorithm/unique/
+
+
+//! from ex 10.9
+void elimdups(std::vector<std::string>& vs)
+{
+    using std::string;
+    std::sort(vs.begin(), vs.end());
+
+    std::for_each(vs.begin(), vs.end(), [](const string& s) { std::cout << s << " "; });
+	std::cout << "\n"; 
+
+    auto new_end = std::unique(vs.begin(), vs.end());
+    vs.erase(new_end, vs.end());
+    
+	std::for_each(vs.begin(), vs.end(), [](const string& s) { std::cout << s << " "; });
+	std::cout << "\n"; 
+}
+
+void biggies(std::vector<std::string>& vs, std::size_t sz)
+{
+    using std::string;
+
+    elimdups(vs);
+
+    //! sort by size, but maintain alphabetical order for same size.
+    std::stable_sort(vs.begin(), vs.end(),
+                     [](string const& lhs, string const& rhs) {
+                         return lhs.size() < rhs.size();
+                     });
+
+    //! get an iterator to the first one whose size() is >= sz
+    auto wc = std::find_if(vs.begin(), vs.end(),
+                           [sz](string const& s) { return s.size() >= sz; });
+
+    //! print the biggies
+    std::for_each(wc, vs.end(), [](const string& s) { std::cout << s << " "; });
+}
+
+int main()
+{
+    //! ex10.16
+    std::vector<std::string> v{"1234", "1234", "1234", "hi~",
+                               "alan", "alan", "cp"};
+    std::cout << "ex10.16: \n";
+    biggies(v, 3);
+    std::cout << std::endl;
+
+    return 0;
+}
+//! output :
+//!
+// ex10.16: hi~ 1234 alan 
